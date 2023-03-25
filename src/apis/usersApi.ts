68d4301ef02c56b6b6c7005c52490b64a1da1122
jonoman55 <jonoman55@yahoo.com>
2023-03-25 13:37:06 -0400

    types and comments added to usersApi

--- src/apis/usersApi.ts
+++ src/apis/usersApi.ts
@@ -3,36 +3,51 @@
 
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+/**
+ * Base API URL
+ */
 const baseUrl: string = `${process.env.REACT_APP_BASE_API_URL}`;
 
+/**
+ * User
+ */
 export type User = {
-  id: number
-  name: string
-  username: string
-  email: string
+  id: number;
+  name: string;
+  username: string;
+  email: string;
   address: {
-    street: string
-    suite: string
-    city: string
-    zipcode: string
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
     geo: {
-      lat: string
-      lng: string
-    }
-  }
-  phone: string
-  website: string
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
   company: {
-    name: string
-    catchPhrase: string
-    bs: string
-  }
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
 };
 
+/**
+ * Get Users API Response
+ */
 type UsersResponse = User[];
 
+/**
+ * Get User By ID API Response
+ */
 type UserResponse = User;
 
+/**
+ * Users API
+ */
 export const usersApi = createApi({
   reducerPath: 'usersApi',
   baseQuery: fetchBaseQuery({
@@ -46,9 +61,9 @@ export const usersApi = createApi({
       }),
       transformResponse: (res: UsersResponse) => res,
     }),
-    getUser: builder.query({
-      query: (id) => ({
-        url: `/users${id}`,
+    getUserById: builder.query({
+      query: (id: number | string) => ({
+        url: `/users/${id}`,
         responseHandler: async (res: Response) => await res.json(),
       }),
       transformResponse: (res: UserResponse) => res,
@@ -58,5 +73,5 @@ export const usersApi = createApi({
 
 export const {
   useGetUsersQuery,
-  useGetUserQuery,
+  useGetUserByIdQuery,
 } = usersApi;
